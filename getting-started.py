b9d2e6e333aea8912af7d18590e2c167b7099b81
Nirakash Shrestha <nirakash.shrestha@gmail.com>
2024-05-20 22:04:35 -0400

    first commit

---
+++ getting-started.py
@@ -0,0 +1,27 @@
+#list
+# print()
+# nums = [1,2,3,4,5]
+
+# print(nums[1])
+
+# print(nums[1:5])
+
+# nums.insert(2,7)
+
+# print(nums)
+
+# nums.pop(4)
+
+# print(nums)
+
+#tuple
+my_tuple=(7,7,1983)
+
+# print(my_tuple[2])
+
+print(x)
+
+
+#set
+
+s = {3,4,5}
